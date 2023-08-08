b703a1c7855b3d88a4f91a71fd5dcd1f286edc47
Durga Sindhu Animalla <duanim@iu.edu>
2023-08-08 00:30:43 -0400

    page2 done completely

--- src/Aboutpage.js
+++ src/Aboutpage.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Aboutpage.css";
-import propic from "./AllImages/propic.jpg";
+import propic from "./AllImages/ratanpropic.jpg";
 import "./Home.css"
 
 function Aboutpage({ projectRef, scrollToSection }) {
   return (
     <div id="about" className="about-container">  
-      <div className="about-content">      
+      <div className="content">
+      <h1 className="about-content__heading">About Me</h1>
         <div className="description">
-          <h1>About Me</h1>
-          <p>Your description goes here.</p>
+        <p>As a Master of Science in Computer Science student at Indiana University Bloomington, I am passionate about developing innovative and user-friendly applications for iOS and Android platforms. I have a strong background in Swift, Python, Java, and MySQL, and I am eager to learn new technologies and frameworks.</p>
+    <p>Previously, I worked as an Associate Software Engineer at Bosch Global Software Technologies, where I handled the full stack development and deployment of the Bosch Levelling Remote App, being the only iOS developer in the team. I also developed a proof-of-concept app that plots the land topography from the data from a Bosch Measuring tool, using Android Studio. Additionally, I have interned at Tata Technologies and Verzeo, where I gained valuable experience in data mining, charting, web development, and RabbitMQ.</p>
+   
         </div>
         <div className="profile-image">
           <img src={propic} alt="Profile" />
